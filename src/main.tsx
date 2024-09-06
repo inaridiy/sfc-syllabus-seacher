946906778c4f2841defec8527c73bd6ca44895bc
inaridiy <inainainariinainari@gmail.com>
2024-09-06 18:20:14 +0900

    feat: tanstack router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,29 @@
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import ReactDOM from "react-dom/client";
 
-const root = document.getElementById("root");
-if (!root) throw new Error("No root element found");
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen";
 
-createRoot(root).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
-);
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
+}
+
+// Render the app
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("No root element found");
+
+if (!rootElement.innerHTML) {
+	const root = ReactDOM.createRoot(rootElement);
+	root.render(
+		<StrictMode>
+			<RouterProvider router={router} />
+		</StrictMode>,
+	);
+}
