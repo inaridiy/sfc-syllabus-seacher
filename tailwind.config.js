417a3e540d55ce9f962b9ee42ebfb3e170b3f64b
inaridiy <inainainariinainari@gmail.com>
2024-09-06 18:57:54 +0900

    style: format

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,9 @@ console.log("tailwind.config.js");
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
